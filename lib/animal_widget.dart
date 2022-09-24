6c69306e63e8e891c73512b330f7a959ca3deff7
Thiago Augusto <wstagf@gmail.com>
2022-09-24 16:53:28 -0300

    estilizado item

--- lib/animal_widget.dart
+++ lib/animal_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 import 'animal.dart';
 
@@ -12,10 +10,49 @@ class AnimalWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
       height: 100,
       width: MediaQuery.of(context).size.width,
-      color: Colors.red,
-      child: Text(animal.name),
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 186, 206, 255),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30.0),
+          bottomLeft: Radius.circular(30.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 5.0,
+            spreadRadius: 5.0,
+            offset: Offset(2.0, 2.0),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Nome: ' + animal.name,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Idade: ' + animal.age.toString()),
+              Text('Sexo: ' + animal.sex),
+              Text('Cor: ' + animal.color),
+              const SizedBox(
+                width: 5,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
