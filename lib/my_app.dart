745ced35e8170ffddc383b6e542a14add30b4bee
Thiago Augusto <wstagf@gmail.com>
2022-09-24 17:50:34 -0300

    Iniciado modo edit

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -24,6 +24,10 @@ class _MyAppState extends State<MyApp> {
 
   bool isLoading = true;
 
+  String flow = "list";
+
+  Animal? selectedAnimal;
+
   @override
   void initState() {
     super.initState();
@@ -85,53 +89,174 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
+  Widget listPage(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : ls.list.isEmpty
+                ? const Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: Text(
+                      'Não existem animais cadastrados, adicione já o primeiro...',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        height: 1.5,
+                      ),
+                    ),
+                  )
+                : SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          for (Animal item in ls.list)
+                            AnimalWidget(
+                                animal: item,
+                                onEdit: () {
+                                  setState(() {
+                                    selectedAnimal = item;
+                                    flow = 'edit';
+                                  });
+                                },
+                                onDelete: () {
+                                  ls.list.removeWhere(
+                                      (element) => element.id == item.id);
+
+                                  updateItensInDB(ls);
+                                }),
+                          Container(
+                            height: 50,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+      ),
+    );
+  }
+
+  Widget editPage(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            TextFormField(
+              initialValue: selectedAnimal!.name,
+              decoration: const InputDecoration(
+                labelText: "Nome",
+                helperText: "Nome do seu animalzinho",
+                helperStyle: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              initialValue: selectedAnimal!.age.toString(),
+              decoration: const InputDecoration(
+                labelText: "Idade",
+                helperText:
+                    "Idade em anos. ex: 18 meses = 1 ano e 6 meses, então digite 1",
+                helperStyle: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              initialValue: selectedAnimal!.sex,
+              decoration: const InputDecoration(
+                labelText: "Sexo",
+                helperText: "Masculino ou Feminino",
+                helperStyle: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              initialValue: selectedAnimal!.color,
+              decoration: const InputDecoration(
+                labelText: "Cor",
+                helperText: "Qual a cor do seu animalzinho",
+                helperStyle: TextStyle(
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      flow = 'list';
+                      selectedAnimal = null;
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Icon(Icons.close),
+                      SizedBox(width: 15),
+                      Text("Cancelar")
+                    ],
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Icon(Icons.save),
+                      SizedBox(width: 15),
+                      Text("Salvar")
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPage(BuildContext context) {
+    if (flow == "list") {
+      return listPage(context);
+    } else {
+      return editPage(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Exemplo de Crud')),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Center(
-          child: isLoading
-              ? const CircularProgressIndicator()
-              : ls.list.isEmpty
-                  ? const Padding(
-                      padding: EdgeInsets.all(25.0),
-                      child: Text(
-                        'Não existem animais cadastrados, adicione já o primeiro...',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 18,
-                          height: 1.5,
-                        ),
-                      ),
-                    )
-                  : SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            for (Animal item in ls.list)
-                              AnimalWidget(
-                                  animal: item,
-                                  onEdit: () {},
-                                  onDelete: () {
-                                    ls.list.removeWhere(
-                                        (element) => element.id == item.id);
-
-                                    updateItensInDB(ls);
-                                  }),
-                            Container(
-                              height: 50,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+      appBar: AppBar(
+          title: const Text(
+        'Exemplo de Crud - Lista de Cachorros',
+        style: TextStyle(
+          fontSize: 14,
         ),
-      ),
+      )),
+      body: buildPage(context),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: AnimatedFloatingActionButton(
         fabButtons: <Widget>[
